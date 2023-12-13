9a92859871c52d7a5651e81f420fc3ed3f8e027e
Barış Adsız <123736772+BarisAdsiz@users.noreply.github.com>
2023-12-13 14:20:15 +0300

    Add files via upload

---
+++ src/ders_5/Ders_5.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ders_5;
+
+/**
+ *
+ * @author Barış Adsız
+ */
+public class Ders_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int sayi = 10;
+        sayi = sayi + 10;
+        
+        int sayi2 = 20;
+        int sonuc = sayi + sayi2;
+        System.out.println(sonuc);
+        
+        
+    }
+    
+}
